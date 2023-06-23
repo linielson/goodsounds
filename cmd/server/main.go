55e9dea3ae5e8da49418247b1b17ceb6b2025d46
Linielson Rosa <linielson.rosa@generalassemb.ly>
2023-06-23 20:53:25 +0200

    feat: read values from env file
    * expose JwtExpiresIn
    * expose TokenAuth
    * initialize config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/linielson/goodsounds/config"
+)
 
+var (
+	logger *config.Logger
+)
+
+func main() {
+	logger = config.NewLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 }
--- config/config.go
+++ config/config.go
@@ -2,34 +2,62 @@ package config
 
 import (
 	"fmt"
+	"github.com/go-chi/jwtauth"
 	"github.com/spf13/viper"
 )
 
+type conf struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JwtExpiresIn  int    `mapstructure:"JWT_EXPIRESIN"`
+	TokenAuth     *jwtauth.JWTAuth
+}
+
 var (
-	logger *Logger
+	config *conf
 )
 
 // Its executed before the main func
 func Init() error {
-	err := loadConfig(".")
+	var err error
+	config, err = loadConfig()
 	if err != nil {
 		return fmt.Errorf("error loading config: ", err)
 	}
 	return nil
 }
 
-func loadConfig(path string) error {
+func loadConfig() (*conf, error) {
+	var cfg *conf
 	viper.SetConfigName("app_config")
 	viper.SetConfigType("env")
-	viper.AddConfigPath(path)
+	viper.AddConfigPath(".")
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
-		return fmt.Errorf("error reading config", err)
+		return nil, fmt.Errorf("error reading config", err)
 	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshal config", err)
+	}
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg, err
+}
 
-	return nil
+func GetTokenAuth() *jwtauth.JWTAuth {
+	return config.TokenAuth
+}
+
+func GetJwtExpiresIn() int {
+	return config.JwtExpiresIn
 }
 
 func GetLogger(p string) *Logger {
