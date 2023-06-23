3b2beefc61c4548269c3dc499b4d116a5c2f896e
Linielson Rosa <linielson.rosa@generalassemb.ly>
2023-06-23 20:27:40 +0200

    feat: setup config to read env file

--- config/config.go
+++ config/config.go
@@ -1,8 +1,36 @@
 package config
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
 var (
 	logger *Logger
 )
+
+// Its executed before the main func
+func Init() error {
+	err := loadConfig(".")
+	if err != nil {
+		return fmt.Errorf("error loading config: ", err)
+	}
+	return nil
+}
+
+func loadConfig(path string) error {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("error reading config", err)
+	}
+
+	return nil
+}
 
 func GetLogger(p string) *Logger {
 	logger := NewLogger(p)
