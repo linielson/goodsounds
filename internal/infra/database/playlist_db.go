1da878f03b4c4de28cd0b81650ce6fb198fd019e
Linielson Rosa <linielson.rosa@generalassemb.ly>
2023-06-23 23:58:59 +0200

    feat: add playlist db findAll

--- internal/infra/database/playlist_db.go
+++ internal/infra/database/playlist_db.go
@@ -16,3 +16,17 @@ func NewPlaylist(db *gorm.DB) *Playlist {
 func (p *Playlist) Create(playlist *entity.Playlist) error {
 	return p.DB.Create(playlist).Error
 }
+
+func (p *Playlist) FindAll(page, limit int, sort string) ([]entity.Playlist, error) {
+	var playlists []entity.Playlist
+	var err error
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+	if page != 0 && limit != 0 {
+		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&playlists).Error
+	} else {
+		err = p.DB.Order("created_at " + sort).Find(&playlists).Error
+	}
+	return playlists, err
+}
--- internal/infra/database/playlist_db_test.go
+++ internal/infra/database/playlist_db_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"github.com/linielson/goodsounds/internal/dto"
 	"github.com/linielson/goodsounds/internal/entity"
 	"github.com/stretchr/testify/assert"
@@ -25,4 +26,39 @@ func TestCreateNewPlaylist(t *testing.T) {
 	err = playlistDB.Create(playlist)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, playlist.ID)
+}
+
+func TestFinalAllPlaylists(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	err = db.AutoMigrate(&entity.Playlist{})
+	if err != nil {
+		t.Error(err)
+	}
+	for i := 1; i < 24; i++ {
+		dataPlaylist := dto.Playlist{Title: fmt.Sprintf("Playlist %d", i), Location: "Italy", Latitude: "45.468016", Longitude: "9.186114"}
+		playlist, err := entity.NewPlaylist(dataPlaylist)
+		assert.NoError(t, err)
+		db.Create(playlist)
+	}
+	playlistDB := NewPlaylist(db)
+	playlists, err := playlistDB.FindAll(1, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, playlists, 10)
+	assert.Equal(t, "Playlist 1", playlists[0].Title)
+	assert.Equal(t, "Playlist 10", playlists[9].Title)
+
+	playlists, err = playlistDB.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, playlists, 10)
+	assert.Equal(t, "Playlist 11", playlists[0].Title)
+	assert.Equal(t, "Playlist 20", playlists[9].Title)
+
+	playlists, err = playlistDB.FindAll(3, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, playlists, 3)
+	assert.Equal(t, "Playlist 21", playlists[0].Title)
+	assert.Equal(t, "Playlist 23", playlists[2].Title)
 }
